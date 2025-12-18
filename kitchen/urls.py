0a045f4341de1ad68722c3cab2dc5ad974d1bb30
Artem <artemp2208@gmail.com>
2025-12-19 01:19:05 +0200

    added dish_list, dish_detail_view to views

--- kitchen/urls.py
+++ kitchen/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
-from kitchen.views import DishListView
+from kitchen.views import DishListView, DishDetailView
 
 urlpatterns = [
+    path("", DishListView.as_view(), name="home"),
     path("dishes/", DishListView.as_view(), name="dish-list"),
+    path("dishes/<int:pk>/", DishDetailView.as_view(), name="dish-detail"),
 ]
--- kitchen/views.py
+++ kitchen/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from kitchen.models import Dish
 
@@ -10,3 +10,9 @@ class DishListView(ListView):
     queryset = Dish.objects.select_related("dish_type").prefetch_related("cooks")
     template_name = "kitchen/dish_list.html"
     context_object_name = "dish_list"
+
+
+class DishDetailView(DetailView):
+    model = Dish
+    template_name = "kitchen/dish_detail.html"
+    context_object_name = "dish"
