d84f62521222e960b72fe09be810e050a99086ef
Artem <artemp2208@gmail.com>
2025-12-18 15:45:12 +0200

    Changed settings, created models and enter them to admin

--- kitchen/admin.py
+++ kitchen/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from kitchen.models import Cook, Dish, DishType
+
+
+@admin.register(Cook)
+class CookAdmin(admin.ModelAdmin):
+    list_display = (
+        "username",
+        "first_name",
+        "last_name",
+        "years_of_experience",
+        "is_staff",
+    )
+    search_fields = ("username", "first_name", "last_name")
+
+
+@admin.register(DishType)
+class DishTypeAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    list_display = ("name", "dish_type", "price")
+    search_fields = ("name",)
+    list_filter = ("dish_type",)
+    filter_horizontal = ("cooks",)
--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class Cook(AbstractUser):
+    years_of_experience = models.PositiveSmallIntegerField(default=0)
+
+    def __str__(self) -> str:
+        return self.username
+
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes",
+    )
+    cooks = models.ManyToManyField(Cook, related_name="dishes")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
