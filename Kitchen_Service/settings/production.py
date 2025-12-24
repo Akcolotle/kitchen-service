6ca3d4c6c6860cb4783b593695e38d732d2c5c46
Artem <artemp2208@gmail.com>
2025-12-24 12:39:53 +0200

    Add STATIC_ROOT for production

--- Kitchen_Service/settings/production.py
+++ Kitchen_Service/settings/production.py
@@ -1,5 +1,6 @@
 import os
 from .base import *
+from pathlib import Path
 
 DEBUG = False
 
@@ -12,13 +13,15 @@ if RENDER_EXTERNAL_HOSTNAME:
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_DB"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": os.getenv("POSTGRES_DB_PORT", 5432),
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
+        "HOST": os.environ["POSTGRES_HOST"],
+        "PORT": os.environ["POSTGRES_DB_PORT"],
         "OPTIONS": {
             "sslmode": "require",
         },
     }
 }
+
+STATIC_ROOT = Path(BASE_DIR) / "staticfiles"
